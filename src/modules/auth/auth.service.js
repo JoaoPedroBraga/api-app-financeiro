eb92c45a3d5d62ff7836aac311c05ee7ece9d79a
Joao Pedro <cls>
2024-03-22 13:18:42 -0300

    FEAT: Finalizaçao rota register

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -16,7 +16,20 @@ export const login = async (params) => {
     }
     // gerar token
     const token = jwt.sign({id:user.id}, process.env.JWT_SECRET)
-        return {token};
+    
+    return {token};
 };
 
-export const register = async (params) => {};
+export const register = async (params) => {
+    const user = await getByEmail(params.email);
+
+    if (user) {
+        return { error: 'Email Cadastrado'};
+    }
+
+    const userCreated = await save(params);
+
+    const token = jwt.sign({id:userCreated[0]}, process.env.JWT_SECRET)
+    
+    return {token};
+};
