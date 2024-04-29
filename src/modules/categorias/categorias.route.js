88797642edf7dc604aecf2b5587157e431b1e45c
Joao Pedro <cls>
2024-04-29 12:45:32 -0300

    FEAT: Criação dad rotas de categoria

--- src/modules/categorias/categorias.route.js
+++ src/modules/categorias/categorias.route.js
@@ -1,36 +1,33 @@
 import { Router } from "express";
-import { getAll, get, save, update, remove, getMe } from '../user/index.js';
+import { getAll, get, save, update, remove } from '../user/index.js';
 import { authMiddleware } from '../../middleware/authMiddleware.js'
 
 export const router = Router();
 
-router.get('/', async (_, res) => {
-    const data = await getAll();
+router.get('/', authMiddleware, async (_, res) => {
+    const data = await getAll(req.user.id);
     res.status(200).json({ data });
 }),
 
-router.get('/me',authMiddleware, async (req, res) => {
-   return res.status(200).json({ data: req.user });
-}),
-
-
-router.get('/:id', async (req, res) => {
-    const data = await get(req.params.id);
+router.get('/:id', authMiddleware, async (req, res) => {
+    const data = await get(req.params.id, req.user.id);
     res.status(200).json({data});
 }),
 
-router.post('/', async (req, res) => {
+router.post('/', authMiddleware, async (req, res) => {
+    req.body.user_id = req.user.id;
     const data = await save(req.body);
     res.status(200).json({data});
 }),
 
-router.delete('/:id', async (req, res) => {
-    const data = await remove(req.params.id);
+router.delete('/:id', authMiddleware, async (req, res) => {
+    const data = await remove(req.params.id, req.user.id);
     res.status(200).json({data});
 }),
 
-router.put('/:id', async (req, res) => {
-    const data = await update(req.params.id, req.body);
+router.put('/:id', authMiddleware, async (req, res) => {
+    req.body.user_id = req.user.id;
+    const data = await update(req.params.id, req.body, req.user.id);
     res.status(200).json({data});
 })
 
--- src/modules/categorias/index.js
+++ src/modules/categorias/index.js
@@ -1,3 +1,2 @@
 export * from './categorias.model.js';
 export * from './categorias.route.js';
-export * from './categorias.service.js'
